e1c0548166c81f9b558fd764b35bd824d135db1e
Steven Keppinger <skeppinger18@gmail.com>
2025-01-19 17:04:50 -0500

    player movement overhaul

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,43 +4,50 @@ extends CharacterBody3D
 # https://docs.godotengine.org/en/stable/getting_started/first_3d_game/03.player_movement_code.html#
 # for reference
 
-# How fast the player moves in meters per second.
-@export var speed = 10
+# How fast the player accelerates in meters per second squared.
+@export var accel = 10
+# The maximum speed the player can be traveling in meters per second.
+@export var max_speed = 30
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
-
-var target_velocity = Vector3.ZERO
+# The base turning speed in degrees per second.
+@export var turning_speed = 60
 
 func _physics_process(delta: float) -> void:
-	# We create a local variable to store the input direction.
-	var direction = Vector3.ZERO
-
-	# We check for each move input and update the direction accordingly.
-	if Input.is_action_pressed("turn_right"):
-		# Rotate the player to the right
-		direction.x += 1
-	if Input.is_action_pressed("turn_left"):
-		# Rotate the player to the left
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		# In 3D, the XZ plane is the ground plane.
-		direction.z += 1
+	
+	# find the direction to the camera
+	var dir_to_cam = global_position.direction_to($Camera3D.global_position)
+	# we only want the direction on the xz plane
+	dir_to_cam.y = 0
+	
+	# move forward -> accelerate away from camera
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		velocity -= dir_to_cam * accel * delta
+	# move backward -> accelerate toward camera
+	if Input.is_action_pressed("move_back"):
+		velocity += dir_to_cam * accel * delta
 	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		# Setting the basis property will affect the rotation of the node.
-		$Pivot.basis = Basis.looking_at(direction)
+	# check which way the player is moving relative to the camera
+	var direction = 0
+	if velocity != Vector3.ZERO:
+		if (velocity.normalized() - dir_to_cam).length() < 1:
+			direction = -1 # the player is moving toward the camera
+		else:
+			direction = 1 # the player is moving away from the camera
 	
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
+	# turn left -> rotate left about y axis
+	if Input.is_action_pressed("turn_left"):
+		rotation_degrees.y += turning_speed * delta
+		velocity = velocity.rotated(Vector3(0, 1, 0), deg_to_rad(turning_speed * delta))
+	# turn right -> rotate right about y axis
+	if Input.is_action_pressed("turn_right"):
+		rotation_degrees.y -= turning_speed * delta
+		velocity = velocity.rotated(Vector3(0, 1, 0), deg_to_rad(-1 * turning_speed * delta))
+	
+	# button to switch side of camera
+	
+	## Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
-	# Moving the Character
-	velocity = target_velocity
+		velocity.y = velocity.y - (fall_acceleration * delta)
+	
 	move_and_slide()
