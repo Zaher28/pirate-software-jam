39cbbba07ce35ccd52cfc5ab35469b4db32c5319
Tyler Pearson <pearsonty9@gmail.com>
2025-01-20 09:26:20 -0500

    Fixed enemies checking position after freed

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -7,18 +7,20 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
-	if target:
-		var direction = target.position - position
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
-		
-		velocity = target_velocity
-		move_and_slide()
+	if !is_instance_valid(self): return
+	if !target: return
 
-		# Check if close enough to deal damage
-		if position.distance_to(target.position) < 5.5:  # Adjust threshold if needed
-			print(target.has_method("take_damage"))
-			print(target)
-			if target.has_method("take_damage"):
-				target.take_damage(damage)
-			queue_free()  # Destroy the enemy after dealing damage
+	var direction = target.position - position
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	velocity = target_velocity
+	move_and_slide()
+
+	# Check if close enough to deal damage
+	if position.distance_to(target.position) < 5.5:  # Adjust threshold if needed
+		print(target.has_method("take_damage"))
+		print(target)
+		if target.has_method("take_damage"):
+			target.take_damage(damage)
+		queue_free()  # Destroy the enemy after dealing damage
