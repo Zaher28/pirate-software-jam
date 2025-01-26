45ee6570479683f2dfff96f67250f2f3513ed97b
Evan Zhang <evanzh15@gmail.com>
2025-01-26 14:15:23 -0500

    Modified AI for enemy
    Enemies have better behavior when avoiding player, can adjust scalers to modify behavior

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -9,20 +9,19 @@ class_name Enemy
 
 enum STATE {
 	NEUTRAL, # Move towards center, not in danger
-	FLEEING # Sees player character, flee for 2 seconds
+	FLEEING # Sees player character, steer away from it
 }
 
+var flee_range = 20 # Higher values cause enemy flee steering force to increase, and vice versa
 var flee_from_target: Node3D = null
-var target_velocity = Vector3.ZERO
 var curr_state = STATE.NEUTRAL
 
 func _ready() -> void:
 	damage = 1
 	ignore_player = false
-	speed = 2
+	speed = 1
 	health = 10
 	$"Cone of Vision".body_entered.connect(is_in_on_sight) # Connects signal to
-	$"Cone of Vision".body_exited.connect(is_out_of_sight) # Connects signal to 
 
 func _physics_process(delta: float) -> void:
 	if !is_instance_valid(self): return
@@ -35,18 +34,34 @@ func _physics_process(delta: float) -> void:
 			target.take_damage(damage)
 		queue_free()  # Destroy the enemy after dealing damage
 	
+	var seek_target_velocity = target.global_position - global_position # Calculate direction towards tower/necromancer
+	seek_target_velocity.y = 0 # Eliminate Y factor from future calculations, theres probably a better way to do this
+	
 	if curr_state == STATE.NEUTRAL:
-		var direction = (target.position - position).normalized() * speed
-		target_velocity.x = direction.x
-		target_velocity.z = direction.z
-		velocity = target_velocity
+		var seek_target_steer = (seek_target_velocity - velocity) * 0.05 # Adjust scalar as needed
+		seek_target_steer.y = 0
+		
+		velocity = (velocity + seek_target_steer).normalized() * speed
 	elif curr_state == STATE.FLEEING:
-		var desired_velocity = flee_from_target.global_position.direction_to(global_position).normalized() * speed
-		#var desired_velocity = (position - flee_from_target.position).normalized() * max_velocity
-		var steering_velocity = (desired_velocity - velocity).limit_length(3)
-		target_velocity.x = steering_velocity.x
-		target_velocity.z = steering_velocity.z
-		velocity = target_velocity
+		var distance_from_player = global_position.distance_to(flee_from_target.global_position) # Obtain distance between player and enemy
+		
+		var avoid_player_velocity = global_position - flee_from_target.global_position # Calculate vector pointing away from player
+		avoid_player_velocity.y = 0
+		if distance_from_player < flee_range:
+			# The further the enemy is from the player, the more it will steer towards tower
+			var seek_target_steer = (seek_target_velocity - velocity) * (distance_from_player / flee_range) 
+			seek_target_steer.y = 0
+			
+			# The closer the enemy is to the player, the more it will steer away from player
+			var avoid_player_steer = (velocity + avoid_player_velocity) * (1 - (distance_from_player / flee_range)) 
+			avoid_player_steer.y = 0
+
+			velocity = (velocity + avoid_player_steer + seek_target_steer).normalized() * speed
+		else: # If player is now outside of the flee_range, revert back to normal behavior until end of Timer
+			var seek_target_steer = (seek_target_velocity - velocity)
+			seek_target_steer.y = 0
+			velocity = (velocity + seek_target_steer).normalized() * speed
+	look_at(velocity) # Have enemy face towards their velocity (which would be towards the tower)
 	move_and_slide()
 
 func is_in_on_sight(body: Node3D) -> void:
@@ -54,13 +69,14 @@ func is_in_on_sight(body: Node3D) -> void:
 	if body.name == "Player":
 		curr_state = STATE.FLEEING
 		flee_from_target = body
-		
-func is_out_of_sight(body: Node3D) -> void:
-	if body.name == "Player":
-		curr_state = STATE.NEUTRAL
-		flee_from_target = null
+	$Timer.start()
 
 func hurt(damage):
 	health -= damage
 	if health <= 0:
 		queue_free()
+
+# After timer expires, set the current state back to neutral, and remove target from target tracking
+func _on_timer_timeout() -> void:
+	curr_state = STATE.NEUTRAL
+	flee_from_target = null
